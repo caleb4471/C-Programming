40b6efcb9efcb4d2da3d9bcf2b2cff15efa466b6
caleb4471 <calebanuri@students.zetech.ac.ke>
2026-05-14 16:45:19 +0300

    Add files via upload
    Caleb Anuri Ngoge.c

---
+++ Week 2 Assignment Task 2.c
@@ -0,0 +1,34 @@
+//Program used to prompt user to enter his/her details
+
+/*
+Description:Program prompting user to enter details
+Name:Caleb Anuri Ngoge
+ADM No:BCS-05-0042/2025
+Date:13/05/2026
+*/
+#include <stdio.h>
+
+int main(){
+	
+	float height;
+	double balance;
+	char phone[20];
+	
+	// User input
+	printf("Enter your height:");
+	scanf("%f", &height);
+	
+	printf("Enter your account balance in Ksh:");
+	scanf("%lf", &balance);
+	
+	printf("Enter your phone number:");
+	scanf("%s", phone);
+	
+	// Output
+	printf("\n----- User details------\n");
+	printf("Height: %.2f\n", height);
+	printf("Bank balance: Ksh %.2lf\n", balance);
+	printf("Phone Number: %s\n", phone);
+	
+	return 0;
+} 
