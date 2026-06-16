6c0d25dff96a30209bd9586b8cc510874709714c
anuri4471 <calebanuri@students.zetech.ac.ke>
2026-06-16 12:06:22 +0300

    Add files via upload

---
+++ Week 4 Task 2, While loop.c
@@ -0,0 +1,29 @@
+//While loop
+
+/* 
+Nmae:Caleb Anuri
+Reg NO:BCS-05-0042/2025
+Date:14/06/2026
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	float bankbalance=10000;
+	float amount;
+	
+	while(bankbalance>0){
+		printf("Enter amount to withdraw:");
+		scanf("%f", &amount);
+		
+		bankbalance = bankbalance - amount;
+		
+		printf("Your bank balance is:%.2f\n", bankbalance);
+		
+	}	
+	
+	printf("You have inadequate funds to withdraw.");
+	
+	return 0;
+}
