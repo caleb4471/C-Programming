a3966a44530026176d3972ab49542f1b1a3318db
anuri4471 <calebanuri@students.zetech.ac.ke>
2026-06-16 12:05:49 +0300

    Add files via upload

---
+++ Week 4 Task 1, For loop.c
@@ -0,0 +1,18 @@
+//For loop
+
+/*
+Name:Caleb Anuri
+Reg NO:BCS-05-0042/2025
+Date:14/06/2026
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int i;
+	for(i=100; i>=50; i--){
+		printf("%d \t", i);
+	}
+	return 0;
+}
